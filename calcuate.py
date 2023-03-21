c56acd01ca83cba4b5801174ab56f7ac8fb469cc
18360979676 <18360979676@163.com>
2023-03-21 13:55:01 +0800

    添加音乐线程

--- calcuate.py
+++ calcuate.py
@@ -1,8 +1,13 @@
+import pygame
 import tkinter
 import win32gui
 import win32con
 import win32com.client
+import os, time
+from tkinter.messagebox import *
 from decimal import Decimal
+from tkinter.filedialog import askdirectory
+from threading import Thread
 
 
 # 验证输入的文本
@@ -12,15 +17,19 @@ def varileText(content):
         if len(price_list) == 3:
             if price_list[2] == 'z':
                 end_price = (Decimal(price_list[0]) * (Decimal(10) + Decimal(
-                    '0.02') * Decimal(price_list[1])) / Decimal(10)).quantize(Decimal('1.1'))
+                    '0.02') * Decimal(price_list[1])) / Decimal(10)).quantize(Decimal('1.11'))
                 label_text.set("平仓价格:{}".format(
                     end_price))
+                T = Thread(target=play)
+                T.start()
                 return True
             elif price_list[2] == 'd':
                 end_price = (Decimal(price_list[0]) * (Decimal(10) - Decimal(
-                    '0.02') * Decimal(price_list[1])) / Decimal(10)).quantize(Decimal('1.1'))
+                    '0.02') * Decimal(price_list[1])) / Decimal(10)).quantize(Decimal('1.11'))
                 label_text.set("平仓价格:{}".format(
                     end_price))
+                T = Thread(target=play)
+                T.start()
                 return True
             else:
                 return True
@@ -33,6 +42,13 @@ def get_all_hand(hwnd, mouse):
         hwnd_map.update({hwnd: win32gui.GetWindowText(hwnd)})
 
 
+def play():
+    pygame.mixer.music.load("resources/audio/error.mp3")
+    pygame.mixer.music.play()
+    time.sleep(1)
+    pygame.mixer.music.stop()
+
+
 if __name__ == '__main__':
     hwnd_map = {}
     # 创建主窗口
@@ -40,12 +56,26 @@ if __name__ == '__main__':
     # 设置标题
     root.title('Liqui_price_computer')
     # 设置主窗口大小
-    root.geometry('200x150+1300+200')
+    root.geometry('578x400+942+200')
     # 设置图标
     root.iconbitmap(
         'resources/a686c9177f3e6709c93db127d88e883df8dcd000ff9b.ico')
     # 设置窗口背景色
-    root['background'] = 'black'
+    root['background'] = '#073642'
+
+
+    # 导入歌曲文件目录
+    # directory = askdirectory()
+    # os.chdir(directory)
+    # song_list = os.listdir()
+    # play_list = tkinter.Listbox(root, font="Helvetica 12 bold", bg='yellow', selectmode=tkinter.SINGLE)
+    # for item in song_list:
+    #     pos = 0
+    #     play_list.insert(pos, item)
+    #     pos += 1
+    pygame.init()
+    pygame.mixer.init()
+
 
     CMD = tkinter.StringVar()
     # 输入---（开仓价格、涨跌倍数、多空方向选择）
@@ -77,6 +107,6 @@ if __name__ == '__main__':
                 # win32gui.SetForegroundWindow(h)
 
                 # 保持当前窗口置顶
-                win32gui.SetWindowPos(h, win32con.HWND_TOPMOST, 1300, 200, 220, 160, win32con.SWP_NOMOVE | win32con.SWP_NOACTIVATE | win32con.SWP_NOOWNERZORDER | win32con.SWP_SHOWWINDOW)
+                win32gui.SetWindowPos(h, win32con.HWND_TOPMOST, 542, 200, 578, 400, win32con.SWP_NOMOVE | win32con.SWP_NOACTIVATE | win32con.SWP_NOOWNERZORDER | win32con.SWP_SHOWWINDOW)
                 
     root.mainloop()
